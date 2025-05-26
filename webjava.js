c8a1f7636eb50cfa0d9e8deec41f1dfa7d194a05
Saarvi Sachdeva <saarvisac35@gmail.com>
2025-05-27 02:24:34 +0530

    final changes

--- webjava.js
+++ webjava.js
@@ -1,51 +1,65 @@
-const galleryy = document.querySelector("#gallery");
+const galleryy = document.querySelector("#card-container");
 const search = document.querySelector("#search")
 
 const gallery = [
-    {id: 1,src:"https://images.unsplash.com/photo-1619164025195-985d31ae8844?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG" , name:"abcd"},
-    {id: 2,src:"https://images.unsplash.com/photo-1619164669943-536b56d813a5?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" , name:"afgh"},
-    {id: 3,src:"https://images.unsplash.com/photo-1542871793-fd7e2b3cd0b8?q=80&w=1504&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" , name:"bjkl"},
-    {id: 4,src:"https://images.unsplash.com/photo-1513127696622-593a20aec127?q=80&w=1526&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" , name:"bnop"},
-    {id: 5,src:"https://images.unsplash.com/photo-1536340314100-5147a6eb2b41?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" , name:"arst"},
-    {id: 6,src:"https://images.unsplash.com/photo-1592966719124-2ca2978ba325?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" , name:"cvwx"},
-    {id: 7,src:"https://images.unsplash.com/photo-1613408548027-2c365217db6f?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" , name:"czab"},
-    {id: 8,src:"https://images.unsplash.com/photo-1592289701772-4a4a8949f8ba?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" , name:"cdef"},
-    {id: 9,src:"https://images.unsplash.com/photo-1610894666077-a714e5a6aa88?q=80&w=1527&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" , name:"ghij"},
-    {id: 10,src:"https://images.unsplash.com/photo-1636262513944-1362edb40902?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" , name:"klmn"},
+    {id: 1,src:"course1.jpg" , name:"Complete Blender Creator" , category:"Development" , ratings:"4.5" , desc:"Learn 3D Modelling for Beginners" , stds:"456,123" , price:"$16.00"},
+    {id: 2,src:"course2.PNG" , name:"Full Stack Fusion" , category:"Design" , ratings:"3.7" , desc:"Learn 3D Modelling for Beginners" , stds:"845,651" , price:"$20"},
+    {id: 3,src:"course3.jpg" , name:"Python Powerhouse" , category:"Development" , ratings:"4.6" , desc:"Learn 3D Modelling for Beginners" , stds:"844,462" , price:"$23"},
+    {id: 4,src:"course4.jpg" , name:"Design to Deploy" , category:"UI/UX" , ratings:"4.0" , desc:"Learn 3D Modelling for Beginners" , stds:"842,369" , price:"$15"},
+    {id: 5,src:"course5.jpg" , name:"SEO Mastery 2025" , category:"Design" , ratings:"4.5" , desc:"Learn 3D Modelling for Beginners" , stds:"658,647" , price:"$30"},
+    {id: 6,src:"course6.jpg" , name:"JavaScript Zero to Hero" , category:"Development" , ratings:"3.9" , desc:"Learn 3D Modelling for Beginners" , stds:"136,899" , price:"$35"},
+    {id: 7,src:"course7.jpg" , name:"AI Unlocked" , category:"Design" , ratings:"4.3" , desc:"Learn 3D Modelling for Beginners" , stds:"875,644" , price:"$30"},
+    {id: 8,src:"course8.jpg" , name:"Data Wizardry" , category:"U/UX" , ratings:"3.5" , desc:"Learn 3D Modelling for Beginners" , stds:"357,159" , price:"$28"}
 ];
 
 var course = "";
 gallery.forEach(function(c){
-    course += `<div class="img"> 
-                <img src="${c.src}"> 
-                <h5>${c.name}</h5> 
-            </div>`
+    course += `<div class="card">
+                <img src="${c.src}" alt="Course Image" class="card-img">
+                <div class="card-body">
+                    <p class="category">${c.category}</p>
+                    <div class="title-rating">
+                    <h3 class="title">${c.name}</h3>
+                    <span class="rating">★ ${c.ratings}</span>
+                    </div>
+                    <p class="desc">${c.desc}</p>
+                    <div class="card-footer">
+                    <span class="students">👤 ${c.stds} students</span>
+                    <span class="price">${c.price}</span>
+                    </div>
+                </div>
+                </div>`
 });
-
 galleryy.innerHTML = course;
 
+
 search.addEventListener("input" , function(){
-    var matching = gallery.filter(function(ss){
-        return ss.name.startsWith(search.value);
+    var matching = gallery.filter(function(output){
+        return output.name.startsWith(search.value);
     });
     var courses = "";
     matching.forEach(function(e){
-        courses += `<div class="img"> 
-                    <img src="${e.src}"> 
-                    <h5>${e.name}</h5> 
+        courses += `<div class="card">
+                <img src="${e.src}" alt="Course Image" class="card-img">
+                <div class="card-body">
+                    <p class="category">${e.category}</p>
+                    <div class="title-rating">
+                    <h3 class="title">${e.name}</h3>
+                    <span class="rating">★ ${e.ratings}</span>
+                    </div>
+                    <p class="desc">${e.desc}</p>
+                    <div class="card-footer">
+                    <span class="students">👤 ${e.stds} students</span>
+                    <span class="price">${e.price}</span>
+                    </div>
+                </div>
                 </div>`
     });
     galleryy.innerHTML = courses;
 });
 
-
-// toggle theme
-function toggleDarkMode() {
-    document.body.classList.toggle("dark-mode");
-  }
-
 //   courses
   function playCourse(courseName) {
     alert("Now playing: " + courseName);
+    window.open('https://youtu.be/lnYZXAIWU8M' , '_blank');
   }
-  
